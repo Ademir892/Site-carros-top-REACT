aa5e7371732ebe95393c7a5faec8764631e11eb7
Ademir Traesel <ademirtraesel@hotmail.com>
2024-08-05 14:50:05 -0300

    Search bar

--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,8 +1,22 @@
+import './styles.css';
 import Header from "../../components/Header";
+import SearchBar from '../../components/Header/SearchBar';
 
-export default function Catalog(){
-
-    return(
+export default function Catalog() {
+  return (
+    <>
         <Header />
-    );
+        <main className='ct-catalog-main'>
+            <section id="ct-catalog-section" className='ct-container'>
+                <div className="ct-serach-bar-container">
+                    <SearchBar />
+                </div>
+
+                <div className="ct-catalog-cards-container">
+
+                </div>
+            </section>
+        </main>
+    </>
+  );
 }
